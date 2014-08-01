b03e2df0ff1cf7f824330d7a7e365dd8a9ac643f
Jared Saul <JaronOrchard@gmail.com>
2014-08-01 15:33:34 -0500

    Mouseover message appears on actionable smelting buttons

--- Crissaegrim/src/smithing/SmeltingRunner.java
+++ Crissaegrim/src/smithing/SmeltingRunner.java
@@ -1,9 +1,11 @@
 package smithing;
 
 import static org.lwjgl.opengl.GL11.*;
+
 import java.io.IOException;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 
 import players.Inventory;
@@ -18,11 +20,16 @@ import crissaegrim.GameRunner;
 import entities.EntityMovementHelper;
 import geometry.Coordinate;
 import geometry.Rect;
+import geometry.RectUtils;
 import gldrawer.GLDrawer;
 
 public class SmeltingRunner {
 	
 	private boolean closeSmeltingRunner;
+	
+	private boolean hasRhichiteOre;
+	private boolean hasValeniteOre;
+	private boolean hasSandelugeOre;
 	
 	private static transient final int BOX_SIZE_PIXELS = 64;
 	private static transient final int INNER_PADDING_PIXELS = 4;
@@ -31,6 +38,9 @@ public class SmeltingRunner {
 	
 	public SmeltingRunner() {
 		closeSmeltingRunner = false;
+		hasRhichiteOre = InventoryUtils.containsOre("Rhichite");
+		hasValeniteOre = InventoryUtils.containsOre("Valenite");
+		hasSandelugeOre = InventoryUtils.containsOre("Sandeluge");
 		recalculateDialogDimensions();
 	}
 	
@@ -55,7 +65,7 @@ public class SmeltingRunner {
 				player.update(); // (Don't negate gravity)
 				gameRunner.drawScene();
 				
-//				int hoveredItemIndex = getHoveredItemIndex();
+				int hoveredItemIndex = getHoveredItemIndex();
 				if (Crissaegrim.getChatBox().isTypingMode()) {
 					Crissaegrim.getChatBox().getKeyboardInput(false);
 				} else {
@@ -64,7 +74,7 @@ public class SmeltingRunner {
 				if (closeSmeltingRunner) { // Close dialog if requested
 					return;
 				}
-//				drawMouseHoverStatus(hoveredItemIndex);
+				drawMouseHoverStatus(hoveredItemIndex);
 //				while (Mouse.next()) {
 //					if (Mouse.getEventButtonState() && Mouse.getEventButton() == 0) {
 //						if (hoveredItemIndex != -1) {
@@ -132,11 +142,42 @@ public class SmeltingRunner {
 		}
 	}
 	
+	/**
+	 * @return The index of the smeltable item the mouse is currently hovering over, or -1 if no item is being hovered over
+	 */
+	private int getHoveredItemIndex() {
+		Coordinate mouseCoords = new Coordinate(Mouse.getX(), Mouse.getY());
+		int posX, posY;
+		posX = (int)(smeltingDialogRect.getLeft()) + OUTER_PADDING_PIXELS;
+		posY = (int)(smeltingDialogRect.getTop()) - OUTER_PADDING_PIXELS*2 - 25;
+		for (int i = 0; i < 2; i++) {
+			Rect smeltableItemBoxRect = new Rect(
+					new Coordinate(posX, posY - BOX_SIZE_PIXELS - INNER_PADDING_PIXELS*2),
+					new Coordinate(posX + BOX_SIZE_PIXELS + INNER_PADDING_PIXELS*2, posY));
+			if (RectUtils.coordinateIsInRect(mouseCoords, smeltableItemBoxRect)) {
+				return i;
+			}
+			posY -= BOX_SIZE_PIXELS + INNER_PADDING_PIXELS*2 + OUTER_PADDING_PIXELS;
+		}
+		return -1;
+	}
+	
+	private void drawMouseHoverStatus(int hoveredItemIndex) {
+		String mouseHoverString;
+		if (hoveredItemIndex == 0 && hasRhichiteOre) {
+			mouseHoverString = "Smelt Rhichite bar";
+		} else if (hoveredItemIndex == 1 && hasValeniteOre && hasSandelugeOre) {
+			mouseHoverString = "Smelt Val_San bar";
+		} else {
+			return; // Nothing to display
+		}
+		TextTexture mouseHoverStatus = Crissaegrim.getCommonTextures().getTextTexture(mouseHoverString);
+		int top = Crissaegrim.getWindowHeight() - 5;
+		GLDrawer.useTexture(mouseHoverStatus.getTextureId());
+		GLDrawer.drawQuad(5, 5 + mouseHoverStatus.getWidth(), top - 20, top);
+	}
+	
 	private void drawSmeltingDialog(Inventory inventory) {
-		boolean hasRhichiteOre = InventoryUtils.containsOre("Rhichite");
-		boolean hasValeniteOre = InventoryUtils.containsOre("Valenite");
-		boolean hasSandelugeOre = InventoryUtils.containsOre("Sandeluge");
-		
 		GameInitializer.initializeNewFrameForWindow();
 		GLDrawer.disableTextures();
 		
